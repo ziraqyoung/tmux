68f1a54211698619570f971633da5659f5dfd331
ziraq young <ziraqyoung@outlook.com>
2024-10-03 21:02:26 +0300

    attempt to fix network

--- scripts/network.sh
+++ scripts/network.sh
@@ -3,7 +3,7 @@
 export LC_ALL=en_US.UTF-8
 
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $current_dir/utils.sh
+source "$current_dir"/utils.sh
 
 # set your own hosts so that a wifi is recognised even without internet access
 HOSTS=$(get_tmux_option "@dracula-network-hosts" "google.com github.com example.com")
@@ -13,11 +13,11 @@ get_ssid()
   # Check OS
   case $(uname -s) in
     Linux)
-      SSID=$(iw dev | sed -nr 's/^\t\tssid (.*)/\1/p')
+      SSID=$(iwgetid -r)
       if [ -n "$SSID" ]; then
-        printf '%s' "$wifi_label$SSID"
+        printf '%s' "  $SSID"
       else
-        echo "$(get_tmux_option "@dracula-network-ethernet-label" "Ethernet")"
+        echo '󰈀 Ethernet'
       fi
       ;;
 
@@ -44,7 +44,7 @@ main()
 {
   network="$(get_tmux_option "@dracula-network-offline-label" "Offline")"
   for host in $HOSTS; do
-    if ping -q -c 1 -W 1 $host &>/dev/null; then
+    if ping -q -c 1 -W 1 "$host" &>/dev/null; then
       network="$(get_ssid)"
       break
     fi
